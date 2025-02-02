0970cafeb170c24f88a46fe1348d744986d11d8c
Leroy Kayanda <leroykayanda@gmail.com>
2025-02-02 19:49:57 +0300

    Test on-demand and amd instances

--- demo-app/terraform/variables.tf
+++ demo-app/terraform/variables.tf
@@ -104,7 +104,7 @@ variable "argocd" {
   default = {
     "staging" = {
       repo_url        = "git@github.com:leroykayanda/eks-cluster.git"
-      target_revision = "1.0.1"
+      target_revision = "1.0.0"
       path            = "_helm-charts/app"
       server          = "staging-argocd.demo.rentrahisi.co.ke:443"
       value_files = [
